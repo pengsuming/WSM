73f6bca4fd5b7a368d7c5583046f8754511a6bee
slhl0116 <w2212@e-mirim.hs.kr>
2023-08-17 10:42:33 +0900

    읽은 날짜에 오늘 날짜 넣자

--- 20230620_bookReviewCalendar/js/review.js
+++ 20230620_bookReviewCalendar/js/review.js
@@ -45,6 +45,16 @@ const publisherDiv = document.getElementsByClassName("publisher")[1];
 publisherDiv.innerHTML = publisher;
 const bookImageDiv = document.getElementsByClassName("book-image")[0];
 bookImageDiv.innerHTML = `<img src="${bookImage}" />`;
+const readDataDiv = document.getElementsByClassName("read-date")[1];
+let now = "2030년 8월 17일";
+now = new Date();   // 현재 날짜와 시각 객체
+let year = now.getFullYear();
+let month = now.getMonth(); // 8월이 7월로 나와. return 실제 월 -1
+let date = now.getDate();
+let day = now.getDay();
 
-
+weeks = ["일", "월", "화", "수", "목", "금", "토"];
+day = weeks[day] + "요일";
+now = `${year}년 ${month+1}월 ${date}일 ${day}`;
+readDataDiv.innerHTML = now;
 
